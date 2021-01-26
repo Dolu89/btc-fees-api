69dc95ec645e91e44a8bd6762cc7a759df3028ed
Dolu <dolu89@gmail.com>
2021-01-26 13:11:13 +0100

    Add ws server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 import express from 'express'
+import WebSocket from 'ws'
 import { getFastetFees } from './src/fees.js'
 const app = express()
 const port = 3000
@@ -8,6 +9,25 @@ app.get('/', async (req, res) => {
   res.json({ fastestFee: await getFastetFees() })
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Bitcoin fees API listening at http://localhost:${port}`)
 })
+
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', function connection (ws) {
+  ws.send('Connected')
+})
+
+async function loopFees () {
+  const fastestFees = await getFastetFees()
+  wss.clients.forEach(async (client) => {
+    if (client.readyState !== WebSocket.OPEN) {
+      return
+    }
+    client.send(JSON.stringify({ fastestFees }))
+  })
+  setTimeout(loopFees, 3000)
+}
+
+loopFees()
